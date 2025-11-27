2a4d34f92424e271e9a2acc42e20b60ca3cf57ae
pveloso <pedrovelosofernandes@outlook.com>
2025-11-27 23:35:21 +0000

    feat(backend): configure JWT authentication and Djoser

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
+    'djoser',
     'api',
 ]
 
