3f451924f5c9e032a62956d33e2eccb4df40c7c3
pveloso <pedrovelosofernandes@outlook.com>
2025-11-28 02:25:46 +0000

    chore: remove old React app directory

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <StoreProvider>
           <ThemeProvider>
-            <CssBaseline />
             {children}
           </ThemeProvider>
         </StoreProvider>
