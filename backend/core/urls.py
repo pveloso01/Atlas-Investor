be2fb2f33dbbd2b889bef0010a811db7d1cd2136
pveloso <pedrovelosofernandes@outlook.com>
2025-12-21 02:25:46 +0000

    feat: create subscription URL routing and integrate with main URLs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("users.urls")),
+    path("api/subscriptions/", include("subscriptions.urls")),
     path("api/", include("api.urls")),
 ]
